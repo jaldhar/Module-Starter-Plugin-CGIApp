dbe593e6003e231773cc1e9ed384b5732a7d4232
Jaldhar H. Vyas <jaldhar@braincells.com>
2008-09-19 03:34:49 +0000

    Tagged Debian version 0.05-1

---
+++ 0.05-1/lib/Module/Starter/Plugin/CGIApp/templates/server.pl
@@ -0,0 +1,15 @@
+use warnings;
+use strict;
+use CGI::Application::Server;
+use lib 'lib';
+use <tmpl_var main_module>;
+
+my $app = <tmpl_var main_module>->new(PARAMS => {
+
+});
+
+my $server = CGI::Application::Server->new();
+$server->entry_points({
+    '/index.cgi' => $app,
+});
+$server->run;
