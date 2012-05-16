019f21c15944b3820f68d9fc5e22a04cd6deeb42
Jaldhar H. Vyas <jaldhar@braincells.com>
2012-05-16 19:30:01 -0400

    0.42   Wed May 16 19:17:15 EDT 2012
            Internal:
            ---------
    
            The license of this module is "Same as Perl" which somewhere along
            the line I thought meant GPL 2+/Artistic 2+ when actually it is
            GPL 1+/Artistic 1+.  This is now fixed in all the relevant places.
    
            In Created distributions:
            -------------------------
    
            The same license confusion occurred here too.  Fixed.

--- t/expected/lib/Foo/Baz.pm
+++ t/expected/lib/Foo/Baz.pm
@@ -144,9 +144,9 @@ This distribution is free software; you can redistribute it and/or modify it
 under the terms of either:
 
 a) the GNU General Public License as published by the Free Software
-Foundation; either version 2, or (at your option) any later version, or
+Foundation; either version 1, or (at your option) any later version, or
 
-b) the Artistic License version 2.0.
+b) the Artistic License version 1.0 or a later version.
 
 The full text of the license can be found in the LICENSE file included
 with this distribution.
